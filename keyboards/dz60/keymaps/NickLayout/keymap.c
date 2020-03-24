dac50b3398fc947d3ccb22c474f4d556f802d0f1
Nesral <nick.larsen31@gmail.com>
2020-03-23 21:10:39 -0500

    Fix the order of the vim keys

--- keyboards/dz60/keymaps/NickLayout/keymap.c
+++ keyboards/dz60/keymaps/NickLayout/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_FL] = LAYOUT_all(
 //QWERTY Funtion layer for toggle:
 //  1        2        3        4        5        6        7        8        9        10       11       12       13       14       15
-    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL, KC_DEL,
+    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_BSPC,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -53,12 +53,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_VI] = LAYOUT_all(
 //QWERTY VIM ARROWS:
 //  1        2        3        4        5        6        7         8        9        10       11       12       13       14       15
-    KC_GRV, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,   KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
-    KC_CAPS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,  KC_TRNS, KC_BSPC,  KC_DEL,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,  KC_TRNS, KC_LEFT,  KC_UP,   KC_DOWN, KC_RIGHT,KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,  KC_TRNS, KC_TRNS,  KC_HOME, KC_PGUP, KC_PGDN, KC_END,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,                    KC_TRNS,          KC_NO,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
-  ),
+    KC_GRV,  KC_NO,   KC_NO,  KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,
+    KC_CAPS, KC_NO,   KC_NO,  KC_NO,   KC_NO,    KC_NO,   KC_BSPC,  KC_DEL,  KC_TRNS, KC_TRNS, KC_NO,   KC_NO,   KC_NO,   RESET,
+    KC_TRNS, KC_NO,   KC_NO,  KC_NO,   KC_NO,    KC_NO,   KC_LEFT,  KC_DOWN, KC_UP,   KC_RIGHT,KC_NO,   KC_NO,   KC_NO,
+    KC_NO,   KC_NO,   KC_NO,  KC_NO,   KC_NO,    KC_NO,   KC_TRNS,  KC_HOME, KC_PGUP, KC_PGDN, KC_END,  KC_NO,   KC_NO,   KC_NO, KC_NO,
+    KC_NO,   KC_NO,   KC_NO,  KC_NO,             KC_NO,                      KC_TRNS,          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO
+    ),
 };
 
 const rgblight_segment_t PROGMEM my_base_layer[] = RGBLIGHT_LAYER_SEGMENTS(
